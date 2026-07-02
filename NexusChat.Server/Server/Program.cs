749c497ce9f3a7a533110fac15c17fc741f4f932
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-07-02 15:21:58 +0600

    feat: add accep connection client

--- NexusChat.Server/Server/Program.cs
+++ NexusChat.Server/Server/Program.cs
@@ -5,6 +5,9 @@ IPEndPoint ipPoint = new(IPAddress.Any, 8888);
 using Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 socket.Bind(ipPoint);
 
+Console.WriteLine(socket.LocalEndPoint);
 socket.Listen(1000);
 
-Console.WriteLine(socket.LocalEndPoint);
+Console.WriteLine("Сервер запущен. Ожидание подключений...");
+using Socket client = await socket.AcceptAsync();
+Console.WriteLine($"Адрес подключенного клиента: {client.RemoteEndPoint}");
