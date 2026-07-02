f786099699e25c2fed7052fe76e5433e503ea1e2
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-07-02 14:27:52 +0600

    feat: add send message

--- NexusChat.Client/Client/Program.cs
+++ NexusChat.Client/Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Sockets;
+using System.Text;
 
 var port = 80;
 var url = "www.google.com";
@@ -6,10 +7,15 @@ var url = "www.google.com";
 using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 try
 {
-	await socket.ConnectAsync(url, port);
+    await socket.ConnectAsync(url, port);
     Console.WriteLine($"Подключение к {url} установлено");
     Console.WriteLine($"Адрес подключения {socket.RemoteEndPoint}");
     Console.WriteLine($"Адрес приложения {socket.LocalEndPoint}");
+
+    var message = $"GET / HTTP/1.1\r\nHost: {url}\r\nConnection: close\r\n\r\n";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    int bytesSent = await socket.SendAsync(messageBytes);
+    Console.WriteLine($"на адрес {url} отправлено {bytesSent} байт(а)");
 
     await socket.DisconnectAsync(true);
 }
