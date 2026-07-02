4d447afc3d92304de219e6d26c259c09ffaa62d4
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-07-02 16:06:08 +0600

    refactor: change creation connection from Socket to TcpClient

--- NexusChat.Client/Client/Program.cs
+++ NexusChat.Client/Client/Program.cs
@@ -6,35 +6,37 @@ var url = "www.google.com";
 var response = await SocketSendRecieveAsync(url, port);
 Console.WriteLine(response);
 
-async Task<Socket?> ConnectSocketAsync(string url, int port)
+async Task<TcpClient?> ConnectSocketAsync(string url, int port)
 {
-    Socket tempSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    TcpClient tcpClient = new();
     try
     {
-        await tempSocket.ConnectAsync(url, port);
+        await tcpClient.ConnectAsync(url, port);
         Console.WriteLine($"Подключение к {url} установлено");
-        Console.WriteLine($"Адрес подключения {tempSocket.RemoteEndPoint}");
-        Console.WriteLine($"Адрес приложения {tempSocket.LocalEndPoint}");
-        return tempSocket;
+        Console.WriteLine($"Адрес подключения {tcpClient.Client.RemoteEndPoint}");
+        Console.WriteLine($"Адрес приложения {tcpClient.Client.LocalEndPoint}");
+        return tcpClient;
     }
     catch (SocketException ex)
     {
         Console.WriteLine(ex.Message);
-        tempSocket.Close();
+        tcpClient.Close();
         Console.WriteLine($"Не удалось установить подключение к {url}");
     }
     return null;
 }
 async Task<string> SocketSendRecieveAsync(string url, int port)
 {
-    using var socket = await ConnectSocketAsync(url, port);
-    if (socket == null)
+    using var tcpClient = await ConnectSocketAsync(url, port);
+    if (tcpClient == null)
         return $"Не удалось установить подключение к {url}";
+
+    NetworkStream stream = tcpClient.GetStream();
 
     var message = $"GET / HTTP/1.1\r\nHost: {url}\r\nConnection: Close\r\n\r\n";
     var messageBytes = Encoding.UTF8.GetBytes(message);
-    int bytesSent = await socket.SendAsync(messageBytes);
-    Console.WriteLine($"на адрес {url} отправлено {bytesSent} байт(а)");
+    await stream.WriteAsync(messageBytes);
+    Console.WriteLine($"на адрес {url} отправлено {messageBytes} байт(а)");
 
     var responseBytes = new byte[512];
     var builder = new StringBuilder();
@@ -42,7 +44,7 @@ async Task<string> SocketSendRecieveAsync(string url, int port)
 
     do
     {
-        bytes = await socket.ReceiveAsync(responseBytes);
+        bytes = await stream.ReadAsync(responseBytes);
         string responsePart = Encoding.UTF8.GetString(responseBytes, 0, bytes);
         builder.Append(responsePart);
     }
