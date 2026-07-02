ffab51582bc68cd1bca9d9f420b82ce5dbff1a99
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-07-02 15:12:41 +0600

    feat: add listen connection to server

--- NexusChat.Server/Server/Program.cs
+++ NexusChat.Server/Server/Program.cs
@@ -5,4 +5,6 @@ IPEndPoint ipPoint = new(IPAddress.Any, 8888);
 using Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 socket.Bind(ipPoint);
 
+socket.Listen(1000);
+
 Console.WriteLine(socket.LocalEndPoint);
