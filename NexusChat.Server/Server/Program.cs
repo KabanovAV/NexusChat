4d07313f9f9857a462ec344524cac2d6446909df
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-07-02 15:11:14 +0600

    feat: add creation of socket

--- NexusChat.Server/Server/Program.cs
+++ NexusChat.Server/Server/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+IPEndPoint ipPoint = new(IPAddress.Any, 8888);
+using Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(ipPoint);
+
+Console.WriteLine(socket.LocalEndPoint);
