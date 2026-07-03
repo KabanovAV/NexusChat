f2297f9c4444361054ef030123c5114539ba0a62
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-07-03 14:25:35 +0600

    refactor: use class message for send and receive

--- NexusChat.Client/Client/Program.cs
+++ NexusChat.Client/Client/Program.cs
@@ -1,5 +1,5 @@
-﻿using System.Net.Sockets;
-using System.Text;
+﻿using Shared;
+using System.Net.Sockets;
 
 using Socket tcpClient = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
@@ -9,38 +9,11 @@ try
     Console.WriteLine($"Подключение к {tcpClient.RemoteEndPoint} установлено");
 
     // Получение сообщения от сервера
-    var lengthBuffer = new byte[4];
-    int byteRead = 0;
-
-    while (byteRead < 4)
-    {
-        int read = await tcpClient.ReceiveAsync(lengthBuffer.AsMemory(byteRead, 4 - byteRead));
-        if (read == 0) throw new SocketException((int)SocketError.ConnectionReset);
-        byteRead += read;
-    }
-
-    int messageLength = BitConverter.ToInt32(lengthBuffer, 0);
-    var responseBytes = new byte[messageLength];
-    int totalBytes = 0;
-
-    while (totalBytes < messageLength)
-    {
-        int bytes = await tcpClient.ReceiveAsync(responseBytes.AsMemory(totalBytes, messageLength - totalBytes));
-        if (bytes == 0) throw new SocketException((int)SocketError.ConnectionReset);
-        totalBytes += bytes;
-    }
-
-    var response = Encoding.UTF8.GetString(responseBytes, 0, totalBytes);
+    var response = await Message.ReceiveMessage(tcpClient);
     Console.WriteLine(response);
 
     // Отправка сообщения на сервера
-    var message = $"Пользователь {tcpClient.LocalEndPoint} отключился";
-    var messageBytes = Encoding.UTF8.GetBytes(message);
-    var lengthHeader = BitConverter.GetBytes(messageBytes.Length);
-
-    await tcpClient.SendAsync(lengthHeader);
-    await tcpClient.SendAsync(messageBytes);
-    await tcpClient.DisconnectAsync(false);
+    await Message.SendMessage(tcpClient, $"Пользователь {tcpClient.LocalEndPoint} отключился");
 }
 catch (SocketException ex)
 {
--- NexusChat.Server/Server/Program.cs
+++ NexusChat.Server/Server/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.Net;
+﻿using Shared;
+using System.Net;
 using System.Net.Sockets;
-using System.Text;
 
 IPEndPoint ipEndPoint = new(IPAddress.Parse("127.0.0.1"), 8888);
 using Socket tcpListener = new(SocketType.Stream, ProtocolType.Tcp);
@@ -15,37 +15,11 @@ try
     Console.WriteLine($"Адрес подключенного клиента: {tcpClient.RemoteEndPoint}");
 
     // Отправка сообщения клиенту
-    var message = "Добро пожаловат в NexusChat";
-    var messageBytes = Encoding.UTF8.GetBytes(message);
-    var lengthHeader = BitConverter.GetBytes(messageBytes.Length); // Получаем 4 байта длины сообщения
-
-    await tcpClient.SendAsync(lengthHeader);
-    int bytesSend = await tcpClient.SendAsync(messageBytes);
+    int bytesSend = await Message.SendMessage(tcpClient, "Добро пожаловат в NexusChat");
     Console.WriteLine($"На адрес {tcpClient.RemoteEndPoint} отправлено {bytesSend} байт(а)");
 
     // Получение сообщения от клиента
-    var lengthBuffer = new byte[4];
-    int byteRead = 0;
-
-    while (byteRead < 4)
-    {
-        int read = await tcpClient.ReceiveAsync(lengthBuffer.AsMemory(byteRead, 4 - byteRead));
-        if (read == 0) throw new SocketException((int)SocketError.ConnectionReset);
-        byteRead += read;
-    }
-
-    int messageLength = BitConverter.ToInt32(lengthBuffer, 0);
-    var responseBytes = new byte[messageLength];
-    int totalBytes = 0;
-
-    while (totalBytes < messageLength)
-    {
-        int read = await tcpClient.ReceiveAsync(responseBytes.AsMemory(totalBytes, messageLength - totalBytes));
-        if (read == 0) throw new SocketException((int)SocketError.ConnectionReset);
-        totalBytes += read;
-    }
-
-    var response = Encoding.UTF8.GetString(responseBytes, 0, totalBytes);
+    var response = await Message.ReceiveMessage(tcpClient);
     Console.WriteLine(response);
 }
 catch (SocketException ex)
